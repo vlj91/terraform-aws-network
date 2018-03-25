c7a67a0dbfb79521ef54d8656542be460de4da26
Vaughan Jones <vaughan.jones@alces-software.com>
2018-03-25 20:29:48 +1100

    Formatting fixes

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 data "aws_availability_zones" "available" {}
 
-resource "aws_vpc" "primary" {
+resource "aws_vpc" "main" {
   enable_dns_hostnames = "${var.enable_dns_hostnames}"
   enable_dns_support   = "${var.enable_dns_support}"
   cidr_block           = "${var.cidr_prefix}.0.0.0/${var.cidr_subnet_mask}"
@@ -8,21 +8,22 @@ resource "aws_vpc" "primary" {
 
 resource "aws_subnet" "public" {
   count             = "${length("${data.aws_availability_zones.available.names}")}"
-  vpc_id            = "${aws_vpc.primary.id}"
+  vpc_id            = "${aws_vpc.main.id}"
   availability_zone = "${element(data.aws_availability_zones.available.names, count.index)}"
   cidr_block        = "${var.cidr_prefix}.0.${count.index}.0/24"
 }
 
 resource "aws_internet_gateway" "main" {
-  vpc_id     = "${aws_vpc.primary.id}"
-  depends_on = ["aws_vpc.primary"]
+  vpc_id     = "${aws_vpc.main.id}"
+  depends_on = ["aws_vpc.main"]
 }
 
 resource "aws_route_table" "main" {
-  vpc_id       = "${aws_vpc.primary.id}"
+  vpc_id = "${aws_vpc.main.id}"
+
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = "${aws_internet_gateway.primary.id}"
+    gateway_id = "${aws_internet_gateway.main.id}"
   }
 }
 
