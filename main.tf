b5d2e1ceef0507d8f00b256866e1f4ff44fc70a8
Vaughan Jones <vaughan.jones@alces-software.com>
2018-04-02 09:29:35 +1000

    Add default security group

--- main.tf
+++ main.tf
@@ -7,10 +7,11 @@ resource "aws_vpc" "main" {
 }
 
 resource "aws_subnet" "public" {
-  count             = "${length("${data.aws_availability_zones.available.names}")}"
-  vpc_id            = "${aws_vpc.main.id}"
-  availability_zone = "${element(data.aws_availability_zones.available.names, count.index)}"
-  cidr_block        = "${var.cidr_prefix}.0.${count.index}.0/24"
+  count                   = "${length("${data.aws_availability_zones.available.names}")}"
+  vpc_id                  = "${aws_vpc.main.id}"
+  availability_zone       = "${element(data.aws_availability_zones.available.names, count.index)}"
+  cidr_block              = "${var.cidr_prefix}.0.${count.index}.0/24"
+  map_public_ip_on_launch = true
 }
 
 resource "aws_internet_gateway" "main" {
@@ -32,4 +33,23 @@ resource "aws_route_table_association" "main" {
   subnet_id      = "${element(aws_subnet.public.*.id, count.index)}"
   route_table_id = "${aws_route_table.main.id}"
   count          = "${length("${data.aws_availability_zones.available.names}")}"
+}
+
+resource "aws_security_group" "default" {
+  description = "Allow inbound SSH"
+  vpc_id      = "${aws_vpc.main.id}"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
 }
